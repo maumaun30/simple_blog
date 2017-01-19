e57019f12eac088606aec04f8f0cecfda5bbe77a
Mau Maun <mau.maun30@gmail.com>
2017-01-19 21:25:36 +0800

    emoticon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,30 +4,40 @@
     <!-- Main Content -->
     <div class="container resp-mgt70">
 
-        @foreach($posts as $post)
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 pdv30 bdt1">
-                <a href="{{ route('posts.show',$post->id) }}">
-                <div class="row">
-                    <div class="col-md-3">
-                        <img src="{{ asset('/img/uploads/thumbnails' . $post->image) }}" alt="{{ $post->image }}" class="img-responsive">
-                    </div>
-                    <div class="col-md-9">
-                        <span class="dp-bl fs30 fw800">
-                            {{ $post->title }}
-                        </span>
-                        <span class="dp-bl fs20 mgv10">
-                            {{ $post->subtitle }}
-                        </span>
-                    </div>
+        @if($posts->isEmpty())
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 pdv30 bdt1 text-center">
+                    <img src="{{ asset('/img/emoticon.gif') }}" class="img-responsive ht100 img-rounded mg-auto">
+                    <br>
+                    <span>No available blogs yet. Why not <a href="{{ route('posts.create') }}">create</a> one?</span>
                 </div>
-                </a>
-                <span class="dp-bl fs15 text-muted pull-right">Posted by <a href="#">{{ $post->postUser->name }}</a> on {{ date_format($post->created_at, 'F d, Y') }}</span>
             </div>
-        </div>
-        @endforeach
+        @else
+            @foreach($posts as $post)
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 pdv30 bdt1">
+                    <a href="{{ route('posts.show',$post->id) }}">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="{{ asset('/img/uploads/thumbnails' . $post->image) }}" alt="{{ $post->image }}" class="img-responsive">
+                        </div>
+                        <div class="col-md-9">
+                            <span class="dp-bl fs30 fw800">
+                                {{ $post->title }}
+                            </span>
+                            <span class="dp-bl fs20 mgv10">
+                                {{ $post->subtitle }}
+                            </span>
+                        </div>
+                    </div>
+                    </a>
+                    <span class="dp-bl fs15 text-muted pull-right">Posted by <a href="#">{{ $post->postUser->name }}</a> on {{ date_format($post->created_at, 'F d, Y') }}</span>
+                </div>
+            </div>
+            @endforeach
 
-        {{ $posts->links() }}
+            {{ $posts->links() }}
+        @endif
 
     </div>
 @stop
